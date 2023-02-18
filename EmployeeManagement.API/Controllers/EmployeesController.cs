253b5aca9ba49d16f98bb44b0a9e85ed62666f5b
buddikatdk <49783759+buddikatdk@users.noreply.github.com>
2023-02-18 15:08:33 +0530

    Add project files.

---
+++ EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeManagement.API.Data;
+using EmployeeManagement.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly EMPDBContext context;
+        public EmployeesController(EMPDBContext context) =>
+            this.context = context;
+
+        [HttpGet]
+        public async Task<IQueryable<Employee>> GetAllEmployeesAsync() =>
+            this.context.Employees.AsQueryable<Employee>();
+
+        [HttpPost]
+        public async ValueTask<Employee> AddEmployeeAsync([FromBody] Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            await this.context.Employees.AddAsync(employee);
+            await this.context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee =
+                await context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id,Employee updateEmployeeRequest)
+        {
+            var employee = await context.Employees.FindAsync(id);
+
+            if(employee is null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await context.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeeManagement.API/Data/EMPDBContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.API.Data
+{
+    public class EMPDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EMPDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
